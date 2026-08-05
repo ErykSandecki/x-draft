7b15c20533487c9159138a5190b3d5c1e1ec4ac4
Eryk Sandecki <eryk.sandecki@gmail.com>
2026-08-05 17:13:54 +0200

    XD: refactor App

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,5 +1,4 @@
-import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 // components
 import App from './App';
@@ -11,25 +10,5 @@ describe('App snapshots', () => {
 
     // result
     expect(asFragment()).toMatchSnapshot();
-  });
-});
-
-describe('App behaviors', () => {
-  it('should toggle the theme attribute when the button is clicked', async () => {
-    // mock
-    const user = userEvent.setup();
-
-    // before
-    render(<App />);
-    const initialTheme = document.documentElement.dataset.theme;
-
-    // find
-    const button = screen.getByRole('button', { name: /switch to/i });
-
-    // action
-    await user.click(button);
-
-    // result
-    expect(document.documentElement.dataset.theme).not.toBe(initialTheme);
   });
 });
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,15 @@
 import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
 
 // core
 import { Routing } from 'core';
 
-// hooks
-import { useTheme } from 'hooks';
-
 // styles
 import styles from './App.module.scss';
 
-const App: FC = () => {
-  const { theme, toggleTheme } = useTheme();
-  const { t } = useTranslation();
-
-  return (
-    <div className={styles.App}>
-      <button type="button" className={styles.ThemeToggle} onClick={toggleTheme}>
-        {theme === 'dark' ? t('app.themeToggle.switchToLight') : t('app.themeToggle.switchToDark')}
-      </button>
-      <Routing />
-    </div>
-  );
-};
+const App: FC = () => (
+  <div className={styles.App}>
+    <Routing />
+  </div>
+);
 
 export default App;
