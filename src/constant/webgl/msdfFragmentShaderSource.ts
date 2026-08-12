39052ceef7d70af3263f8d3edfaf6c9bb9480e23
Eryk Sandecki <eryk.sandecki@gmail.com>
2026-08-12 09:18:48 +0200

    XD: font fix

--- src/constant/webgl/msdfFragmentShaderSource.ts
+++ src/constant/webgl/msdfFragmentShaderSource.ts
@@ -14,10 +14,8 @@ float median(float r, float g, float b) {
 void main() {
   vec3 msdf = texture(u_texture, v_texCoord).rgb;
   float signedDist = median(msdf.r, msdf.g, msdf.b) - 0.5;
-  float screenPxDistance = u_screenPxRange * signedDist;
-  float opacity = clamp(screenPxDistance + 0.5, 0.0, 1.0);
-  float contrast = mix(1.8, 1.0, clamp(u_screenPxRange / 2.0, 0.0, 1.0));
-  opacity = clamp((opacity - 0.5) * contrast + 0.5, 0.0, 1.0);
+  float aa = max(fwidth(signedDist), 1e-4);
+  float opacity = clamp(signedDist / aa + 0.5, 0.0, 1.0);
 
   outColor = vec4(u_color.rgb, u_color.a * opacity);
 }
